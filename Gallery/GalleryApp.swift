ae605f07bf07fac417d2f3c3cd0cf64fd8e3f8e5
Asmaa Abdelfattah <109594626+AsmaaAbdelfattah@users.noreply.github.com>
2025-05-17 00:31:54 +0300

    Initial Commit

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by asma abdelfattah on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
